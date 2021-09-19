a0e85752bfd1688a3401b7618e70800ed16a0364
Radhejha <radhemohanjha.cse19@chitkarauniversity.edu.in>
2021-09-19 22:17:10 +0530

    adding ract

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./Components/Hero/Nav";
+import Last from "./Components/Footer/Last";
+import Mobile from "./Components/Restorent/Mobile";
+import Order from "./Components/OrderTrack/Order";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Order />
+      <Mobile />
+      <Last />
     </div>
   );
 }
